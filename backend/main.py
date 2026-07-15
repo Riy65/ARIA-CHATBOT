c90f18496670bfb9dd72eeda20adfb61459da03b
Riy65 <riyarpit71325@gmail.com>
2026-07-15 19:47:48 +0530

    fix CORS issue

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://aria-chatbot-rust.vercel.app/"],
+    allow_origins=["https://aria-chatbot-rust.vercel.app","http://127.0.0.1:5500",
+        "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
