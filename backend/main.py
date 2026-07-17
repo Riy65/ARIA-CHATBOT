dc70c2a4dfd9e8edffac2e7d879e8db5a245a53b
Riy65 <riyarpit71325@gmail.com>
2026-07-17 13:42:44 +0530

    again Update CORS for OpenAI frontend

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://aria-chatbot-61l6a9vhq-riy65s-projects.vercel.app","http://127.0.0.1:5500",
+    allow_origins=["https://aria-chatbot-git-openai-version-riy65s-projects.vercel.app/","http://127.0.0.1:5500",
         "http://localhost:5500"],
     allow_credentials=True,
     allow_methods=["*"],
