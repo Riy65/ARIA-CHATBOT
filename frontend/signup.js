18c977407e133c8441e004a3a7afd650b111e3f7
Riy65 <riyarpit71325@gmail.com>
2026-07-17 13:49:00 +0530

    updated js files

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,6 +1,6 @@
 const signupForm = document.getElementById("signupForm");
 const signupBtn = document.getElementById("signupBtn");
-const API_URL= "https://aria-chatbot-1shq.onrender.com/"
+const API_URL= "https://aria-chatbot-1shq.onrender.com"
 
 signupForm.addEventListener("submit", async function (e) {
 
